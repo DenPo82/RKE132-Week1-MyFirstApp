9d9d7f6c58748d15d9d0dab50d2ca90acdc37de4
DenPo82 <savyour@gmail.com>
2022-09-07 01:25:02 +0300

    Create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,13 @@
+﻿// This is a comment to my code
+
+//Programm küsib kasutajalt tema nime ja siis tervitab tema nimepidi
+
+Console.WriteLine("Enter Your Name:");
+
+//string- sõne
+string userName = Console.ReadLine();
+
+Console.WriteLine("Hello" + ", " + userName + "!");
+
+//string interpolation
+Console.WriteLine($"Hello, {userName}!");
